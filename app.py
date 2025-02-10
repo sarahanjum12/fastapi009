cc54f3825592876fb820af2b2dc7038878c4d2a6
sarahanjum12 <127437565+sarahanjum12@users.noreply.github.com>
2025-02-11 04:56:16 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],  # Change to specific frontend origin if needed
+    allow_origins=["*"],  # Change to specific frontend origin if needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
